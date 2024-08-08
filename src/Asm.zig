dc18ba810952d39e1e5da0b1c1c69461b496cba4
Felix <29074327+felix-u@users.noreply.github.com>
2024-08-08 00:35:51 -0400

    many vork

---
+++ src/Asm.zig
@@ -0,0 +1,169 @@
+const Bici = @import("Bici.zig");
+const std = @import("std");
+
+pub const max_filesize = 8 * 1024 * 1024;
+
+var out = [_]u8{0} ** (0x10000 - 0x100);
+var pc: u16 = 0;
+
+var forward_res = [_]u16{0} ** 0x100;
+var forward_res_idx: u8 = 0;
+
+pub fn compile(allocator: std.mem.Allocator, path_asm_file: []const u8) ![]const u8 {
+    const assembly = try std.fs.cwd().readFileAlloc(allocator, path_asm_file, max_filesize);
+    var i: usize = 0;
+    var add: usize = 1;
+    while (i < assembly.len) : (i += add) {
+        add = 1;
+        if (std.ascii.isWhitespace(assembly[i])) continue;
+
+        var mode_k: u8 = 0;
+        var mode_r: u8 = 0;
+        var mode_2: u8 = 0;
+
+        switch (assembly[i]) {
+            '#' => {
+                i += 1;
+                if (i == assembly.len) {
+                    std.log.err("expected hex digit, found EOF at '{s}'[{d}]", .{ path_asm_file, i });
+                    return error.InvalidSyntax;
+                }
+
+                const beg_i = i;
+                if (!std.ascii.isHex(assembly[i])) {
+                    std.log.err(
+                        "expected hex digit, found byte '{c}'/{d}/#{x} at '{s}'[{d}]",
+                        .{ assembly[i], assembly[i], assembly[i], path_asm_file, i },
+                    );
+                    return error.InvalidSyntax;
+                }
+
+                while (i < assembly.len and std.ascii.isHex(assembly[i])) {
+                    i += 1;
+                }
+                const end_i = i;
+                switch (end_i - beg_i) {
+                    2 => {
+                        try compileOp(0, 0, 0, @intFromEnum(Bici.Op.push));
+                        try write(try std.fmt.parseUnsigned(u8, assembly[beg_i..end_i], 16));
+                    },
+                    4 => {
+                        try compileOp(0, 0, 1, @intFromEnum(Bici.Op.push));
+                        try write2(try std.fmt.parseUnsigned(u16, assembly[beg_i..end_i], 16));
+                    },
+                    else => {
+                        std.log.err(
+                            "expected 2 digits (byte) or 4 digits (short), found {d} digits in number at '{s}'[{d}..{d}]",
+                            .{ end_i - beg_i, path_asm_file, beg_i - 1, end_i },
+                        );
+                        return error.InvalidNumber;
+                    },
+                }
+                continue;
+            },
+            '{' => {
+                forward_res[forward_res_idx] = pc;
+                forward_res_idx += 1;
+                pc += 1;
+                add = 2; // TODO: handle more forward resolution modes than just '#', which we're skipping over here
+                continue;
+            },
+            '}' => {
+                // TODO: more forward resolution modes
+                i += 1;
+                forward_res_idx -= 1;
+                const res_pc = forward_res[forward_res_idx];
+                out[res_pc] = @intCast(pc - 1 - res_pc);
+                continue;
+            },
+            '"' => {
+                i += 1;
+                if (i == assembly.len) {
+                    std.log.err("expected string, found EOF at '{s}'[{d}]", .{ path_asm_file, i });
+                    return error.InvalidSyntax;
+                }
+
+                while (i < assembly.len and !std.ascii.isWhitespace(assembly[i])) : (i += 1) {
+                    try write(assembly[i]);
+                }
+                add = 0;
+                continue;
+            },
+            '\'' => {
+                i += 1;
+                if (i == assembly.len) {
+                    std.log.err("expected byte, found EOF at '{s}'[{d}]", .{ path_asm_file, i });
+                    return error.InvalidSyntax;
+                }
+
+                try write(assembly[i]);
+                continue;
+            },
+            else => {},
+        }
+
+        const beg_i = i;
+        i += 1;
+        var end_i = i;
+
+        while (i < assembly.len and !std.ascii.isWhitespace(assembly[i])) : ({
+            i += 1;
+            end_i = i;
+        }) {
+            if (assembly[i] != ';') continue;
+
+            i += 1;
+            while (i < assembly.len and !std.ascii.isWhitespace(assembly[i])) : (i += 1) switch (assembly[i]) {
+                'k' => mode_k = 1,
+                'r' => mode_r = 1,
+                '2' => mode_2 = 1,
+                else => {
+                    std.log.err(
+                        "expected one of ['k', 'r', '2'], found byte '{c}'/{d}/#{x} at '{s}'[{d}]",
+                        .{ assembly[i], assembly[i], assembly[i], path_asm_file, i },
+                    );
+                    return error.InvalidSyntax;
+                },
+            };
+
+            break;
+        }
+
+        const lexeme = assembly[beg_i..end_i];
+        for (std.meta.tags(Bici.Op)) |tag| {
+            if (std.mem.eql(u8, lexeme, @tagName(tag))) {
+                try compileOp(mode_k, mode_r, mode_2, @intFromEnum(tag));
+                break;
+            }
+        } else inline for (@typeInfo(Bici.Instruction.special).Struct.decls) |op| {
+            if (std.mem.eql(u8, lexeme, op.name)) {
+                try write(@field(Bici.Instruction.special, op.name));
+                break;
+            }
+        } else {
+            std.log.err("invalid token '{s}' at '{s}'[{d}..{d}]", .{ lexeme, path_asm_file, beg_i, end_i });
+            return error.InvalidSyntax;
+        }
+
+        continue;
+    }
+
+    return out[0..pc];
+}
+
+fn compileOp(mode_k: u8, mode_r: u8, mode_2: u8, op: u8) !void {
+    try write(op | (mode_2 << 5) | (mode_r << 6) | (mode_k << 7));
+}
+
+fn write(byte: u8) !void {
+    out[pc] = byte;
+    pc = std.math.add(u16, pc, 1) catch |e| {
+        std.log.err("exceeded maximum program size", .{});
+        return e;
+    };
+}
+
+fn write2(short: u16) !void {
+    try write(@truncate((short & 0xff00) >> 8));
+    try write(@truncate(short));
+}
---
+++ src/Bici.zig
@@ -0,0 +1,168 @@
+const std = @import("std");
+
+pub const Op = enum(u5) {
+    push,
+    drop,
+    nip,
+    swap,
+    rot,
+    dup,
+    over,
+    @"=",
+    @"!=",
+    @">",
+    @"<",
+    @"+",
+    @"-",
+    @"*",
+    @"/",
+    @"++",
+    not,
+    @"and",
+    @"or",
+    xor,
+    shift,
+    jmp,
+    jeq,
+    jst,
+    stash,
+    load,
+    loadr,
+    store,
+    storer,
+    read,
+    write,
+};
+
+pub const Instruction = packed struct {
+    op: Op,
+    mode: packed struct {
+        keep: bool = false,
+        stack: enum(u1) { param, ret } = .param,
+        size: enum(u1) { byte, short } = .byte,
+    } = .{},
+
+    pub const special = struct {
+        pub const jmi: u8 = @bitCast(Instruction{ .op = .push, .mode = .{ .keep = true } });
+        pub const jei: u8 = @bitCast(Instruction{ .op = .push, .mode = .{ .keep = true, .size = .short } });
+        pub const jsi: u8 = @bitCast(Instruction{ .op = .push, .mode = .{ .keep = true, .stack = .ret } });
+        pub const @"break": u8 = @bitCast(Instruction{ .op = .push, .mode = .{ .keep = true, .stack = .ret, .size = .short } });
+    };
+};
+
+var memory = [_]u8{0} ** 0x10000;
+
+var stacks: struct {
+    param: [0x100]u8 = [_]u8{0} ** 0x100,
+    param_ptr: u8 = 0,
+    ret: [0x100]u8 = [_]u8{0} ** 0x100,
+    ret_ptr: u8 = 0,
+
+    pub fn setParam(self: *@This()) void {
+        stack = &self.param;
+        stack_ptr = &self.param_ptr;
+    }
+
+    pub fn setRet(self: *@This()) void {
+        stack = &self.ret;
+        stack_ptr = &self.ret_ptr;
+    }
+} = .{};
+var stack = &stacks.param;
+var stack_ptr = &stacks.param_ptr;
+
+var keep = false;
+
+pub fn run(bytecode: []const u8) !void {
+    std.debug.print("BYTECODE ===\n", .{});
+    for (bytecode, 0..) |byte, i| std.debug.print(
+        "[{d:2}]\t'{c}'\t#{x:02}\t{s}\n",
+        .{ i, byte, byte, @tagName(@as(Op, @enumFromInt(byte & 0x1f))) },
+    );
+    std.debug.print("\nRUN ===\n", .{});
+    defer std.debug.print(
+        "\nSTACKS ===\nparam:\t{x:02}\nreturn:\t{x:02}\n",
+        .{ stacks.param[0..stacks.param_ptr], stacks.ret[0..stacks.ret_ptr] },
+    );
+
+    @memcpy(memory[0x100..][0..bytecode.len], bytecode);
+    var i: u16 = 0x100;
+
+    while (i < 0x10000) : (i += 1) {
+        switch (memory[i]) {
+            Instruction.special.jmi => return error.Todo,
+            Instruction.special.jei => return error.Todo,
+            Instruction.special.jsi => return error.Todo,
+            Instruction.special.@"break" => break,
+            else => {},
+        }
+
+        const instruction: Instruction = @bitCast(memory[i]);
+
+        switch (instruction.mode.stack) {
+            .param => stacks.setParam(),
+            .ret => stacks.setRet(),
+        }
+        keep = instruction.mode.keep;
+
+        switch (instruction.mode.size) {
+            .byte => switch (instruction.op) {
+                .push => {
+                    i +%= 1;
+                    push(memory[i]);
+                },
+                .@"+" => push(pop() + pop()),
+                .write => {
+                    // TODO: device table in memory[0x00..0x100] (special-cased for now)
+                    std.debug.assert(pop() == 0x00);
+                    i +%= 1;
+                    const strlen = memory[i];
+                    std.debug.print("{s}", .{memory[i + 1 ..][0..strlen]});
+                    i +%= strlen;
+                },
+                else => {
+                    std.log.err("todo: {any}", .{instruction});
+                    return error.Todo;
+                },
+            },
+            .short => switch (instruction.op) {
+                .push => {
+                    i +%= 1;
+                    push2(u16LittleEndianAt(&memory, i));
+                    i +%= 1;
+                },
+                .@"+" => push2(pop2() + pop2()),
+                else => {
+                    std.log.err("todo: {any}", .{instruction});
+                    return error.Todo2;
+                },
+            },
+        }
+    }
+}
+
+fn u16LittleEndianAt(bytes: []const u8, idx: anytype) u16 {
+    return @as(u16, bytes[idx]) | @as(u16, bytes[idx +% 1]) << 8;
+}
+
+fn pop() u8 {
+    const popped = stack[stack_ptr.* -% 1];
+    if (!keep) stack_ptr.* -%= 1;
+    return popped;
+}
+
+fn pop2() u16 {
+    const popped = u16LittleEndianAt(stack, stack_ptr.* -% 2);
+    if (!keep) stack_ptr.* -%= 2;
+    return popped;
+}
+
+fn push(value: u8) void {
+    stack[stack_ptr.*] = value;
+    stack_ptr.* +%= 1;
+}
+
+fn push2(value: u16) void {
+    push(@truncate(value >> 8));
+    push(@truncate(value));
+}

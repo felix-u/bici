3196a37ada6996c0fddcc9eee6d6f90d3ce6b5ff
Felix <29074327+felix-u@users.noreply.github.com>
2025-01-17 01:30:50 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,51 +1,5 @@
 @echo off
 
-REM @echo off
-REM
-REM setlocal
-REM set name=bici
-REM
-REM for %%a in (%*) do set "%%a=1"
-REM if not "%msvc%"=="1" if not "%clang%"=="1" set msvc=1
-REM if not "%release%"=="1"                    set debug=1
-REM
-REM set dir_deps=..\deps
-REM set dir_sdl=%dir_deps%\SDL2-2.30.6-win32
-REM set include_paths=-I%dir_sdl%\include
-REM
-REM set cl_common=cl -nologo -FC -diagnostics:column -std:c11 %include_paths%
-REM set clang_common=clang -std=c11 %include_paths%
-REM
-REM set cl_link=-link -incremental:no -subsystem:console %dir_sdl%\lib\x64\SDL2main.lib %dir_sdl%\lib\x64\SDL2.lib shell32.lib
-REM set clang_link=
-REM
-REM set cl_debug=%cl_common% -W4 -WX -Z7 -DBUILD_DEBUG=1
-REM REM -fsanitize=address
-REM set clang_debug=%clang_common% -pedantic ^
-REM     -Wall -Werror -Wextra -Wshadow -Wconversion -Wdouble-promotion ^
-REM     -Wno-unused-function -Wno-sign-conversion -Wno-deprecated-declarations -fno-strict-aliasing ^
-REM     -g3 -fsanitize=address,undefined -fsanitize-trap -DBUILD_DEBUG=1
-REM
-REM set cl_out=-out:
-REM set clang_out=-o
-REM
-REM if "%msvc%"=="1"  set compile_debug=%cl_debug%
-REM if "%msvc%"=="1"  set compile_link=%cl_link%
-REM if "%msvc%"=="1"  set compile_out=%cl_out%
-REM
-REM if "%clang%"=="1" set compile_debug=%clang_debug%
-REM if "%clang%"=="1" set compile_link=%clang_link%
-REM if "%clang%"=="1" set compile_out=%clang_out%
-REM
-REM if not exist build mkdir build
-REM pushd build
-REM
-REM if not exist SDL2.dll copy %dir_sdl%\lib\x64\SDL2.dll .
-REM if exist %name%.pdb del %name%.pdb
-REM %compile_debug% ..\src\main.c %compile_link% %compile_out%%name%.exe || exit /b 1
-REM
-REM popd
-
 setlocal
 set name=bici
 
